2753b8d7a19ba087bdf668d3ec10d2f803a6246b
Victor Elezua <victorelezua@gmail.com>
2020-10-07 05:47:18 +0100

    add graphql middleware

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Start");
+require('dotenv').config();
+const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
+const schema = require('./schema');
+
+const PORT = process.env.SERVER_PORT;
+
+const app = express();
+
+app.use("/graphql", graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}));
+
+
+app.get("/", (req, res) => {
+    res.send("Hello Express");
+});
+
+app.listen(PORT, () => {
+    console.log("Server is running at " + PORT);
+    console.log("Dependent service: " + process.env.DEV_API_URL);
+});
